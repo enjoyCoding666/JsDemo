7e5cc1e9489d4128ef847fd7bcf65cd176cd3deb
fenglin <huaiyinnianshao28@163.com>
2017-10-13 17:14:11 +0800

    Initial commit

---
+++ base/externalJs.js
@@ -0,0 +1,7 @@
+/**
+ * Created by lenovo on 2017/10/13.
+ */
+
+function printText() {
+     document.getElementById("prama2").innerHTML="通过函数打印文本";
+}
---
+++ base/varOperation.js
@@ -0,0 +1,10 @@
+/**
+ * Created by lenovo on 2017/10/13.
+ */
+function   add( number1, number2) {
+    return number1+number2;
+}
+
+function subtract(number1,number2) {
+    return number1-number2;
+}
